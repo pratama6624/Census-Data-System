904b33452860ae07183c8eef80c79b5ed02ae049
pratama6624 <scyoung6624@gmail.com>
2025-11-09 23:27:51 +0700

    Setup JWT Package, env & JWT_SECRET

--- backend-vapor/Sources/backend-vapor/configure.swift
+++ backend-vapor/Sources/backend-vapor/configure.swift
@@ -2,19 +2,28 @@ import NIOSSL
 import Fluent
 import FluentMySQLDriver
 import Vapor
+import JWT
+import JWTKit
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-app.databases.use(DatabaseConfigurationFactory.mysql(
+    app.databases.use(DatabaseConfigurationFactory.mysql(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? MySQLConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database"
     ), as: .mysql)
+    
+    // Ambil secret dari .env (kalau belum ada pakai default dev)
+    let secret = Environment.get("JWT_SECRET") ?? "dev-secret-change-me"
+
+    // Daftarkan HS256 key ke JWT (convert String to HMACKey)
+    let hmacKey = HMACKey(from: Data(secret.utf8))
+    await app.jwt.keys.add(hmac: hmacKey, digestAlgorithm: .sha256)
 
     app.migrations.add(CreateTodo())
 
